48985987d28cc5618c4595a8fa7d5642bed506c1
sun1 <sun1@>
2019-02-26 18:12:11 +0800

    sun1-1

--- sun1.py
+++ sun1.py
@@ -1,3 +1,11 @@
 a = 1
 
 b = 2
+
+
+设置记住密码（默认15分钟）：
+git config --global credential.helper cache
+如果想自己设置时间，可以这样做(1小时后失效)：
+git config credential.helper 'cache --timeout=3600'
+长期存储密码：
+git config --global credential.helper store
